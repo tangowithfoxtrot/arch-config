19aa4c0b839049258a6af8a46b3e4658f13e1b43
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2022-09-25 17:30:36 -0700

    works on my system ¯\_(ツ)_/¯

--- start.sh
+++ start.sh
@@ -2,5 +2,12 @@
 
 set -e
 
+# set arch_config_dir to the directory where arch-config is cloned
+ansible localhost, -c local -m ansible.builtin.blockinfile -a \
+"path=$(pwd)/imperative_deletion.sh \
+marker='### {mark} ANSIBLE-MANAGED BLOCK ###' \
+block='arch_config_dir=$(pwd)' insertafter='set -e'"
+
 ansible-galaxy collection install -r requirements.yml
-ansible-playbook -i localhost, -c local --become --ask-become-pass main.yml
+ansible-playbook -i localhost, -c local --become --ask-become-pass \
+main.yml
