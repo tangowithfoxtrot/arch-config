2855b822abb6c77ffeae3a96f1ca27ce0efbc28b
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2022-09-27 13:53:02 -0700

    fixed log-redirection

--- imperative_deletion.sh
+++ imperative_deletion.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
 ### BEGIN ANSIBLE-MANAGED BLOCK ###
 ### END ANSIBLE-MANAGED BLOCK ###
 
@@ -11,10 +10,14 @@ imperative_packages=$(cat /etc/pkglist.yml /etc/pkglist_aur.yml | sed 's/  - //g
 remove_packages=$(diff -w --unchanged-line-format= --old-line-format= --new-line-format='%L' \
     <(echo "$declarative_packages" | sed 's/ /\n/g') <(echo "$imperative_packages"))
 
-# remove imperative packages that are not in declarative packages
-if [[ -n "$remove_packages" ]]; then
-    echo "$(date)" >> /var/log/imperative_deletion.log
-    echo "Removing packages:" >> /var/log/imperative_deletion.log
-    echo "$remove_packages" >> /var/log/imperative_deletion.log
-    pacman -Rns --noconfirm $remove_packages &>> /var/log/imperative_deletion.log
-fi
+main() {
+    if [[ -n "$remove_packages" ]]; then
+        echo "$(date)" 2>&1 | tee -a /var/log/imperative_deletion.log
+        echo "Removing the following packages:" 2>&1 | tee -a /var/log/imperative_deletion.log
+        echo "$remove_packages" 2>&1 | tee -a /var/log/imperative_deletion.log
+        pacman -Rns --noconfirm $remove_packages 2>&1 | tee -a /var/log/imperative_deletion.log && \
+        return 0
+    fi
+}
+
+main "$@"
